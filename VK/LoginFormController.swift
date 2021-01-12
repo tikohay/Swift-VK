4da114c2cf0abe99ab17b4ca4ef13147ccfbf860
Levon Karakhanyan <levon98@bk.ru>
2021-01-12 16:42:04 +0300

    prepared to pr

--- VK/LoginFormController.swift
+++ VK/LoginFormController.swift
@@ -18,14 +18,33 @@ class LoginFormController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        addCornerRadius()
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    private func addCornerRadius() {
         logoVK.layer.cornerRadius = 20
         logoVK.clipsToBounds = true
 
         loginButton.layer.cornerRadius = 10
         loginButton.clipsToBounds = true
-        
-        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        scrollView?.addGestureRecognizer(hideKeyboardGesture)
     }
     
     @objc func keyboardWasShown(notification: Notification) {
@@ -41,22 +60,6 @@ class LoginFormController: UIViewController {
     @objc func keyboardWillBeHidden() {
         let contentInsets = UIEdgeInsets.zero
         scrollView?.contentInset = contentInsets
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
     }
 
     @objc func hideKeyboard() {
