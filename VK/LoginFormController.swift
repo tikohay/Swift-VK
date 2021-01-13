787137b94d09f57564ab215bb017c7dc8a8de74d
Levon Karakhanyan <levon98@bk.ru>
2021-01-13 15:41:49 +0300

    done

--- VK/LoginFormController.swift
+++ VK/LoginFormController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class LoginFormController: UIViewController {
-
+    
     @IBOutlet weak var logoVK: UIImageView!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
@@ -16,61 +16,28 @@ class LoginFormController: UIViewController {
     @IBOutlet weak var passwordVK: UITextField!
     
     @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
-        
     }
+
+    private var keyboardShown = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        logoVK.layer.cornerRadius = 20
-        logoVK.clipsToBounds = true
-
-        loginButton.layer.cornerRadius = 10
-        loginButton.clipsToBounds = true
         
-        
-        
-        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        prepareElementsToView()
+        addTapGestureRecognizer()
     }
     
-    
-    
-    @objc func keyboardWasShown(notification: Notification) {
-        let info = notification.userInfo! as NSDictionary
-        let kbsize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbsize.height, right: 0.0)
-        
-
-        self.scrollView?.contentInset = contentInsets
-        scrollView?.scrollIndicatorInsets = contentInsets
-    }
-
-    @objc func keyboardWillBeHidden() {
-        let contentInsets = UIEdgeInsets.zero
-        scrollView?.contentInset = contentInsets
-    }
-
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
-    }
-
-    @objc func hideKeyboard() {
-        self.scrollView?.endEditing(true)
+        
+        addKeyboardObservers()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        removeKeyboardObservers()
+    }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         let checkResult = checkUserData()
@@ -81,7 +48,42 @@ class LoginFormController: UIViewController {
         return checkResult
     }
     
-    func checkUserData() -> Bool {
+    private func prepareElementsToView() {
+        logoVK.layer.cornerRadius = 20
+        logoVK.clipsToBounds = true
+        
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
+    }
+    
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWasShown),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillBeHidden),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+        
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardDidHideNotification,
+                                                  object: nil)
+    }
+    
+    private func addTapGestureRecognizer() {
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView?.addGestureRecognizer(hideKeyboardGesture)
+    }
+    
+    private func checkUserData() -> Bool {
         guard let login = loginVK.text, let password = passwordVK.text else {
             return false
         }
@@ -93,10 +95,32 @@ class LoginFormController: UIViewController {
         }
     }
     
-    func showLoginError() {
+    private func showLoginError() {
         let alert = UIAlertController(title: "Ошибка", message: "вы ввели неправильные данные", preferredStyle: .alert)
         let action = UIAlertAction(title: "ok", style: .cancel, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    @objc private func keyboardWasShown(notification: Notification) {
+        guard !keyboardShown else { return }
+        let info = notification.userInfo! as NSDictionary
+        let kbsize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbsize.height, right: 0.0)
+        
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+        keyboardShown = true
+    }
+    
+    @objc private func keyboardWillBeHidden() {
+        guard keyboardShown else { return }
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+        keyboardShown = false
+    }
+    
+    @objc private func hideKeyboard() {
+        self.scrollView?.endEditing(true)
     }
 }
