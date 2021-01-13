6224ab2014b55e5582dc67179b1490ad7e43e310
Levon Karakhanyan <levon98@bk.ru>
2021-01-13 12:03:46 +0300

    done

---
+++ VK/PhotoFriend/PhotoFriendController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoFriendController.swift
+//  VK
+//
+//  Created by Karahanyan Levon on 12.01.2021.
+//
+
+import UIKit
+
+class PhotoFriendController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VK/PhotoFriend/PhotoOfFriendCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoOfFriendCell.swift
+//  VK
+//
+//  Created by Karahanyan Levon on 12.01.2021.
+//
+
+import UIKit
+
+class PhotoOfFriendCell: UICollectionViewCell {
+    
+}
