237b66ae3f245cb81c39eddc5e2e74b40c2fba52
Levon Karakhanyan <levon98@bk.ru>
2021-06-01 16:13:03 +0300

    done lesson7

--- VK/News/NewsTableViewCell.swift
+++ VK/News/NewsTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class NewsTableViewCell: UITableViewCell {
-
+    
     var commentText: String?
     var isLike = false
     var likeCount = 0
@@ -29,6 +29,14 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var commentsLabel: UILabel?
     @IBOutlet weak var likeButton: UIButton?
     @IBOutlet weak var commentButton: UIButton?
+    @IBOutlet weak var readButton: UIButton?
+    @IBOutlet weak var labelHeightAnchor: NSLayoutConstraint?
+    
+    var isReaded = false {
+        didSet {
+            layoutSubviews()
+        }
+    }
     
     @IBAction func likeTapped(_ sender: UIButton) {
         if !isLike {
@@ -50,16 +58,77 @@ class NewsTableViewCell: UITableViewCell {
         
     }
     
+    @IBAction func readButtonTapped(_ sender: UIButton) {
+        if !isReaded {
+            newsLabelFrame()
+            readButton?.setTitle("Read less", for: .normal)
+            isReaded.toggle()
+        } else {
+            newsLabelFrame()
+            readButton?.setTitle("Read more", for: .normal)
+            isReaded.toggle()
+        }
+    }
+    
     func set(newsMain: NewsGroup, newsFill: NewsItem) {
         UIImageView.getPhoto(from: newsFill.attachments?.first?.photo?.sizes.last?.url ?? "", imageView: newsImage!)
         UIImageView.getPhoto(from: newsMain.image, imageView: newsLogoImage!)
         newsNameLabel?.text = newsMain.name
         newsTextLabel?.text = newsFill.text
+        
+        checkIfNeededReadButton()
+    }
+    
+    func getLabelSize(text: String, font: UIFont) -> CGFloat {
+        
+        let maxWidth = textLabel!.frame.width
+        
+            let textBlock = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
+        let rect = text.boundingRect(with: textBlock, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+            let width = Double(rect.size.width)
+            let height = Double(rect.size.height)
+            let size = CGSize(width: ceil(width), height: ceil(height))
+        return size.height
+    }
+    
+    private func checkIfNeededReadButton() {
+        guard
+            let text = newsTextLabel?.text,
+            let font = newsTextLabel?.font
+        else { return }
+        
+        let height = getLabelSize(text: text, font: font)
+        
+        if height > CGFloat(100) {
+            readButton?.isHidden = false
+        } else {
+            readButton?.isHidden = true
+        }
+    }
+    
+    private func newsLabelFrame() {
+        guard
+            let text = newsTextLabel?.text,
+            let font = newsTextLabel?.font
+        else { return }
+        
+        let height = getLabelSize(text: text, font: font)
+        
+        if !isReaded {
+            labelHeightAnchor?.constant = 100
+        } else {
+            labelHeightAnchor?.constant = height
+        }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         commentsLabel?.text = commentText
         newsTextScrollView?.showsVerticalScrollIndicator = false
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        newsLabelFrame()
     }
 }
